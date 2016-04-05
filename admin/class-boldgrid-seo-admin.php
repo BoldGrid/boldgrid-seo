8c3117e6e8a2e44adc697a245c4bf4f53d04625f
Tim <dev@tim.ph>
2016-04-05 15:14:58 -0400

    fixing undefined index

--- admin/class-boldgrid-seo-admin.php
+++ admin/class-boldgrid-seo-admin.php
@@ -287,25 +287,11 @@ class Boldgrid_Seo_Admin {
 	 * @since 	1.0.0
 	 * @return 	string 	Page Title - Blog Name
 	 */
-	public function boldgrid_titles(  ) {
-
-		if ( ! isset( $_GET['menu'] ) ) {
-
-			if ( isset( $_GET['action'] )
-
-				&& $_GET['action'] === 'edit' ) :
-
-					return apply_filters( 'the_title', get_the_title( $_GET['post'] ) ) .
-
-					' - ' .
-
-					get_bloginfo( 'name' );
-
-			endif;
-
-		}
-
-	}
+	 public function boldgrid_titles(  ) {
+		 if ( 'edit' !== isset( $_GET['action'] ) && isset( $_GET['post'] ) ) {
+			 return apply_filters( 'the_title', get_the_title( $_GET['post'] ) ) . ' - ' . get_bloginfo( 'name' );
+		 }
+	 }
 
 
 
