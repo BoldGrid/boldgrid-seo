d05d86a0673a807df21cff182b7890d35714a0ea
Joe C <joe@css-solutions.com>
2016-04-12 13:10:43 -0400

    WPB-1816: Fixed update class interference with non-boldgrid plugin information pages.

--- includes/class-boldgrid-seo-update.php
+++ includes/class-boldgrid-seo-update.php
@@ -73,7 +73,8 @@ class Boldgrid_Seo_Update {
 
 			// Is page for plugin information?
 			$is_plugin_information = ( 'plugin-install.php' === $pagenow && isset( $_GET['plugin'] ) &&
-				 isset( $_GET['tab'] ) && 'plugin-information' === $_GET['tab'] );
+				 'boldgrid-seo' === $_GET['plugin'] && isset( $_GET['tab'] ) &&
+				 'plugin-information' === $_GET['tab'] );
 
 			// Is this a plugin update action?
 			$is_plugin_update = ( isset( $_REQUEST['action'] ) &&
