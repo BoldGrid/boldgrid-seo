ad2b8fd0372f0fecee67b9a3f72567cdc5ec8483
Tim <dev@tim.ph>
2016-08-20 14:15:57 -0400

    remove meta-field configs

--- includes/configs/meta-field.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	'separator_icon'      => '&gt;',
-	'breadcrumbs_id'      => 'breadcrumbs',
-	'breadcrumbs_classes' => 'breadcrumb-trail breadcrumbs',
-	'home_title'          => 'Home',
-);
