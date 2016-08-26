2814cd34e77430df1f35facc95e07fcc775cc70e
Tim <dev@tim.ph>
2016-08-26 11:24:25 -0400

    resize metabox fields with css instead of js

--- assets/js/boldgrid-seo-admin.js
+++ assets/js/boldgrid-seo-admin.js
@@ -13,7 +13,6 @@ BOLDGRID.SEO = BOLDGRID.SEO || {};
 		init : function () {
 			$( document ).ready( function() {
 				self._setWordCounts();
-				self._setMetaBoxDisplay();
 			});
 		},
 
@@ -22,11 +21,11 @@ BOLDGRID.SEO = BOLDGRID.SEO || {};
 				$counter   = $( '<span />', {
 					'class' : 'boldgrid-seo-meta-counter',
 					'style' : 'font-weight: bold'
-				} ),
+				}),
 				$container = $( '<div />', {
 					'class' : 'boldgrid-seo-meta-countdown boldgrid-seo-meta-extra',
 					'html'  : ' characters left'
-				} );
+				});
 
 			if ( limit ) {
 				 $element
@@ -34,7 +33,7 @@ BOLDGRID.SEO = BOLDGRID.SEO || {};
 					.after( $container.prepend( $counter ) )
 					.on( 'keyup focus' , function() {
 						self.setCounter( $counter, $element, limit );
-					} );
+					});
 			}
 
 			self.setCounter( $counter, $element, limit );
@@ -43,7 +42,7 @@ BOLDGRID.SEO = BOLDGRID.SEO || {};
 		// Set the counter element for meta description and meta title
 		setCounter : function( $counter, $target, limit ) {
 			var text  = $target.val(),
-				chars = text.length;
+			    chars = text.length;
 
 			$counter.html( limit - chars );
 
@@ -56,49 +55,6 @@ BOLDGRID.SEO = BOLDGRID.SEO || {};
 			} else {
 				$counter.css( { 'color' : 'black' } );
 			}
-		},
-
-		_setMetaBoxDisplay : function() {
-			var $document = $( document ),
-			    $meta_title_height,
-			    $wp_screen_columns;
-
-			// Meta Title's textarea's height to set
-			$meta_title_height = $( '#boldgrid-seo-field-meta_title' );
-			// Screen Column's partial selector.
-			$wp_screen_columns = '#adv-settings > div.columns-prefs > .columns-prefs-';
-
-			// Check Screen Column tab > Screen Layout and see if the single column radio is marked.
-			if( $( $wp_screen_columns + '1 input:radio:checked' ).length > 0 ) {
-				// Set to a height of one since it's full width.
-				$meta_title_height.attr( 'rows', '1' );
-			// Or assume that 2 columns is used since we only have a choice of 1 or 2 columns.
-			} else {
-				// Set to a height of two since it's much more compact.
-				$meta_title_height.attr( 'rows', '2' );
-			}
-
-			// Delegate event to the 1 column radio to listen for change and activate trigger.
-			$document.delegate( $wp_screen_columns + '1 input:radio:checked',
-				// What to do if change happens.
-				'change', function( event ) {
-					// We will set the height of the meta title section to one row.
-					$meta_title_height.attr( 'rows', '1' );
-				}
-			);
-
-			// Delegate event to the 2 column radio to listen for change and activate trigger.
-			$document.delegate( $wp_screen_columns + '2 input:radio:checked',
-				// What to do if change happens.
-				'change', function( event ) {
-					// We will set the height of the meta title section to two rows.
-					$meta_title_height.attr( 'rows', '2' );
-				}
-			);
-
-			// Trigger events on change.
-			$( $wp_screen_columns + '2 input:radio:checked' )
-				.trigger( 'change' );
 		},
 
 		// Set the WordCounts in the SEO Metabox
