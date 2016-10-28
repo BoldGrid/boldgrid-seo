0cbf4e866fc3929adbd61f0b9ed1e2d93296236b
Tim <dev@tim.ph>
2016-10-28 10:56:39 -0400

    adding reading ease scoring

--- includes/configs/i18n/contentanalysis.config.php
+++ includes/configs/i18n/contentanalysis.config.php
@@ -52,8 +52,12 @@ return array(
 		),
 	),
 	'readingEase' => array(
-		'good' => 'Your content\'s readability is great! It\'s easily understood by the majority of readers!',
-		'ok' => 'Your content looks good, but you could simplify the text a bit to make it more readable to a wider audience.',
-		'bad' => 'The content is very hard to read, and is best understood by university graduates.  You should consider making your sentences shorter and using easier words for people to understand.',
+		'goodHigh' => 'Your content\'s readability is looking great! It\'s very easy to understand by the majority of readers!',
+		'goodMedHigh' => 'The readability of your content is easy to understand by most readers! Awesome!',
+		'goodMedLow' => 'Your content is fairly easy to understand by most readers.',
+		'goodLow' => 'The readability of this content is okay, but could use some improvements to reach a wider audience.',
+		'ok' => 'Your content is pretty hard to read, so you should try to simplify it.',
+		'badHigh' => 'The content is hard to read. Try shortening some sentences and using less complex wording to improve this score.',
+		'badLow' => 'The text here is very hard to read, and is best understood by university graduates.  You should consider making your sentences shorter and using easier words for people to understand.',
 	),
 );
