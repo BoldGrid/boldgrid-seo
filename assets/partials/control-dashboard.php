46c24ba6a011ad45c89e21abb9335c189704503b
Tim <dev@tim.ph>
2016-10-27 22:34:45 -0400

    adding content length score to template

--- assets/partials/control-dashboard.php
+++ assets/partials/control-dashboard.php
@@ -1,4 +1,28 @@
-
+<div class="bgseo-recommendations">
+	<# if ( ! _.isUndefined( data.content ) ) { #>
+		<# if ( ! _.isUndefined( data.content.lengthScore ) ) { #>
+			<span class="analysis-suggestion {{{ data.content.lengthScore.status }}}">
+				{{{ data.content.lengthScore.msg }}}
+			</span>
+		<# } #>
+	<# } #>
+</div>
+<div class="bgseo-recommendations">
+	<# if ( ! _.isUndefined( data.title ) ) { #>
+		<span class="analysis-suggestion {{{ data.title.lengthScore.status }}}">
+			{{{ data.title.lengthScore.msg }}}
+		</span>
+	<# } #>
+</div>
+<div class="bgseo-recommendations">
+	<# if ( ! _.isUndefined( data.description ) ) { #>
+		<# if ( ! _.isUndefined( data.description.lengthScore ) ) { #>
+			<span class="analysis-suggestion {{{ data.description.lengthScore.status }}}">
+				{{{ data.description.lengthScore.msg }}}
+			</span>
+		<# } #>
+	<# } #>
+</div>
 <# if ( ! _.isUndefined( data.textstatistics ) ) { #>
 	<# if ( ! _.isUndefined( data.textstatistics.readingEase ) ) { #>
 	<div class="bgseo-keywords">Reading Ease: {{{ data.textstatistics.readingEase }}}</div>
@@ -10,23 +34,4 @@
 		<# } #>
 	</div>
 	<# } #>
-
 <# } #>
-
-<div class="bgseo-recommendations">
-	<# if ( ! _.isUndefined( data.title ) ) { #>
-		<span class="analysis-suggestion {{{ data.title.lengthScore.status }}}">
-			{{{ data.title.lengthScore.msg }}}
-		</span>
-	<# } #>
-</div>
-
-<div class="bgseo-recommendations">
-	<# if ( ! _.isUndefined( data.description ) ) { #>
-		<# if ( ! _.isUndefined( data.description.lengthScore ) ) { #>
-			<span class="analysis-suggestion {{{ data.description.lengthScore.status }}}">
-				{{{ data.description.lengthScore.msg }}}
-			</span>
-		<# } #>
-	<# } #>
-</div>
