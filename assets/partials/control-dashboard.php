2be0789b02e9fb4fa9d3e3f98d6631c470e325e3
Tim <dev@tim.ph>
2016-10-28 05:42:55 -0400

    reducing template complexity

--- assets/partials/control-dashboard.php
+++ assets/partials/control-dashboard.php
@@ -1,28 +1,12 @@
-<div class="bgseo-recommendations">
-	<# if ( ! _.isUndefined( data.content ) ) { #>
-		<# if ( ! _.isUndefined( data.content.lengthScore ) ) { #>
-			<span class="analysis-suggestion {{{ data.content.lengthScore.status }}}">
-				{{{ data.content.lengthScore.msg }}}
-			</span>
-		<# } #>
-	<# } #>
-</div>
-<div class="bgseo-recommendations">
-	<# if ( ! _.isUndefined( data.title ) ) { #>
-		<span class="analysis-suggestion {{{ data.title.lengthScore.status }}}">
-			{{{ data.title.lengthScore.msg }}}
+<# _.each( data, function( recommendation ) { #>
+	<# if ( ! _.isUndefined( recommendation.lengthScore ) ) { #>
+	<div class="bgseo-recommendations">
+		<span class="analysis-suggestion {{{ recommendation.lengthScore.status }}}">
+			{{{ recommendation.lengthScore.msg }}}
 		</span>
+	</div>
 	<# } #>
-</div>
-<div class="bgseo-recommendations">
-	<# if ( ! _.isUndefined( data.description ) ) { #>
-		<# if ( ! _.isUndefined( data.description.lengthScore ) ) { #>
-			<span class="analysis-suggestion {{{ data.description.lengthScore.status }}}">
-				{{{ data.description.lengthScore.msg }}}
-			</span>
-		<# } #>
-	<# } #>
-</div>
+<# } ); #>
 <# if ( ! _.isUndefined( data.textstatistics ) ) { #>
 	<# if ( ! _.isUndefined( data.textstatistics.readingEase ) ) { #>
 	<div class="bgseo-keywords">Reading Ease: {{{ data.textstatistics.readingEase }}}</div>
