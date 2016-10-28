dea7a9258fe62d0b0ba15f6a2ca4ed178c1b3a37
Tim <dev@tim.ph>
2016-10-27 22:31:54 -0400

    adding content lenght status messages

--- includes/configs/i18n/contentanalysis.config.php
+++ includes/configs/i18n/contentanalysis.config.php
@@ -37,4 +37,12 @@ return array(
 			'good' => __( 'Your title doesn\'t use any stop words that will negatively impact your SEO ranking! Good Job!', 'bgseo' ),
 		),
 	),
+	'content' => array(
+		'length' => array(
+			'badEmpty'  => __( 'You haven\'t entered any content yet!  As you start writing your content, recommendations for better SEO will be made!', 'bgseo' ),
+			'badShort'  => __( 'The content should be longer, try writing more relevant information about the focus keyword of this page.', 'bgseo' ),
+			'ok'   => __( 'We recommend a minimum of 300 words for the best SEO results.', 'bgseo' ),
+			'good' => __( 'Your content is over the recommended minimum of 300 words, good job!', 'bgseo' ),
+		),
+	),
 );
