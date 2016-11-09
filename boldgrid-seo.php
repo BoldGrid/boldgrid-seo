4deb76b0c31fdfd9a04859910b30d00633eaca5b
Tim <dev@tim.ph>
2016-11-09 14:07:03 -0500

    updating plugin textdomain

--- boldgrid-seo.php
+++ boldgrid-seo.php
@@ -19,7 +19,7 @@
  * Author URI: http://www.boldgrid.com
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: boldgrid-seo
+ * Text Domain: bgseo
  * Domain Path: /languages
  */
 
