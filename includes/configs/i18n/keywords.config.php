07623c345ff96adb679c56114f7a09a187920fdb
Tim <dev@tim.ph>
2016-11-11 14:12:22 -0500

    adding keywords translations

---
+++ includes/configs/i18n/keywords.config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'recommendedKeyword' => array(
+		'template' => '%s: <b>%s</b>.',
+		'text'  => __( 'Based on your content and frequency, search engines will likely think your content is about', 'bgseo' ),
+		'setNewTarget' => __( 'Set a new target keyword below, and the dashboard will be updated with new stats!', 'bgseo' ),
+	),
+);
