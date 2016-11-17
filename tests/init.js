de7b76be1315959081f2b525234c2a0baf28dee7
Tim <dev@tim.ph>
2016-11-17 13:05:04 -0500

    adding init test cases

---
+++ tests/init.js
@@ -0,0 +1,8 @@
+describe( 'Initialization Test', function() {
+	it( "Checks that api.Init is defined.", function() {
+		expect( BOLDGRID.SEO.Init ).toBeDefined();
+	});
+	it( "Checks that api.Init.load() is defined.", function() {
+		expect( BOLDGRID.SEO.Init.load ).toBeDefined();
+	});
+});
