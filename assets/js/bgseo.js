7c75efb2a975195dd44141c01809b94e095ea06e
Tim <dev@tim.ph>
2016-11-17 23:25:01 -0500

    updating inline js doc blocks for tooltips

--- assets/js/bgseo.js
+++ assets/js/bgseo.js
@@ -2748,29 +2748,67 @@ BOLDGRID.SEO = BOLDGRID.SEO || {};
 			$( document ).ready( self.onReady );
 		},
 
+		/**
+		 * Sets up event listeners and selector cache in settings on document ready.
+		 *
+		 * @since 1.3.1
+		 */
 		onReady : function() {
 			self.getSettings();
 			self.hideToolTips();
 			self._enableTooltips();
 			self._toggleTooltip();
 		},
+
+		/**
+		 * Cache selectors
+		 *
+		 * @since 1.3.1
+		 */
 		getSettings : function() {
 			self.settings = {
 				description : $( '.butterbean-control .butterbean-description' ),
 				tooltip  : $( '<span />', { 'class' : 'bgseo-tooltip dashicons dashicons-editor-help', 'aria-expanded' : 'false' }),
 			};
 		},
+
+		/**
+		 * Toggle Tooltips
+		 *
+		 * This sets up the event listener for clicks on tooltips or control labels,
+		 * which will hide and show the description of the control for the user.
+		 *
+		 * @since 1.3.1
+		 */
 		_toggleTooltip : function() {
 			$( '.butterbean-label, .bgseo-tooltip' ).on( 'click', function( e ) {
 				self.toggleTooltip( e );
 			});
 		},
+
+		/**
+		 * Enables tooltips for any controls that utilize the description field.
+		 *
+		 * @since 1.3.1
+		 */
 		_enableTooltips : function() {
 			self.settings.description.prev().append( self.settings.tooltip );
 		},
+
+		/**
+		 * This handles the toggle of the tooltip open/close.
+		 *
+		 * @since 1.3.1
+		 */
 		toggleTooltip : function( e ) {
 			$( e.currentTarget ).next( '.butterbean-description' ).slideToggle();
 		},
+
+		/**
+		 * This hides all tooltips when api.Tooltips is initialized.
+		 *
+		 * @since 1.3.1
+		 */
 		hideToolTips : function() {
 			self.settings.description.hide();
 		},
