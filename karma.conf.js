6c1ce60070984f868b48369883900fb3bcba3995
Tim <dev@tim.ph>
2016-12-09 14:22:11 -0500

    added recommended keyword test

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
     files: [
       'https://code.jquery.com/jquery-1.12.4.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
+      'tests/test-helpers.js',
       'assets/js/bgseo/boldgrid-seo-util.js',
       'assets/js/bgseo/boldgrid-seo-title.js',
       'assets/js/bgseo/boldgrid-seo-description.js',
@@ -14,7 +15,6 @@ module.exports = function(config) {
       'assets/js/bgseo/boldgrid-seo-tooltips.js',
       'assets/js/bgseo/boldgrid-seo-keywords.js',
       'assets/js/bgseo/boldgrid-seo-init.js',
-      'tests/test-helpers.js',
       'tests/bgseo/*.js'
     ]
   });
--- tests/bgseo/keywords.js
+++ tests/bgseo/keywords.js
@@ -15,6 +15,19 @@
 			expect( word ).not.toBe( "he'd" );
 		});
 
+		it( 'Recommended keyword is found in content snippet.', function() {
+			// Set document ready.
+			var word, content;
+			content = "If you have any questions feel free to message us. We will get back to you as soon as we can. If you have any questions feel free to message us. We will get back to you as soon as we can.  havent havent havent havent havent havent havent havent";
+			// Convert to array for recommended Keywords search.
+			content = content.split( ' ' );
+			word = BOLDGRID.SEO.Keywords.recommendedKeywords( content, 1 );
+			// This appears more frequently.
+			expect( word[0][0] ).not.toBe( "havent" );
+			// This should be the actual found word despite havent used more.
+			expect( word[0][0] ).toBe( 'questions' );
+		});
+
 	});
 
 })( jQuery );
