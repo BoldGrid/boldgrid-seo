7ad20050248367495f910f2f9a1bc5ec3fecc9fd
Tim <dev@tim.ph>
2016-12-14 12:21:28 -0500

    capitalize SEO Title

--- includes/configs/i18n/seoTitle.config.php
+++ includes/configs/i18n/seoTitle.config.php
@@ -3,7 +3,7 @@ return array(
 	'length' => array(
 		'badEmpty'  => __( 'You haven\'t entered a custom SEO Title to your page, you should consider adding one.', 'bgseo' ),
 		'badLong'  => __( 'Your custom SEO Title is longer than the recommended 70 characters, you should consider making it shorter.', 'bgseo' ),
-		'ok'   => __( 'We suggest making your SEO title at least 30 characters.', 'bgseo' ),
+		'ok'   => __( 'We suggest making your SEO Title at least 30 characters.', 'bgseo' ),
 		// Max value.
 		'okScore' => 30,
 		'good' => __( 'Your SEO Title is a good length, and optimized for Search Engines!', 'bgseo' ),
@@ -11,7 +11,7 @@ return array(
 		'goodScore' => 70,
 	),
 	'keywordUsage' => array(
-		'bad'  => __( 'You should try to use your focus keyword at least one time in your SEO title.', 'bgseo' ),
+		'bad'  => __( 'You should try to use your focus keyword at least one time in your SEO Title.', 'bgseo' ),
 		'ok'   => __( 'It’s great you’ve used the focus keyword in your title, but you should try to only use that one time.', 'bgseo' ),
 		'good' => __( 'Your SEO Title is optimized by using your focus keyword!', 'bgseo' ),
 	),
