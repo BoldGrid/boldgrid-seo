e669d15fdcbae7c39ad27ee3ad6c7891f15f98ae
Tim <dev@tim.ph>
2017-01-04 16:41:30 -0500

    add content length inline links

--- includes/configs/i18n/content.config.php
+++ includes/configs/i18n/content.config.php
@@ -3,14 +3,30 @@ return array(
 	'length' => array(
 		/** Translators: %s is the total Word Count of the content. **/
 		'contentLength' => __( 'Word Count: %s.', 'bgseo' ),
-		'badEmpty'  => __( 'You haven\'t entered any content yet!  As you start writing your content, we\'ll make recommendations for better SEO!', 'bgseo' ),
-		'badShort'  => __( 'The content should be longer, try writing more about the focus keyword of this page.', 'bgseo' ),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'badEmpty'  => sprintf( __( 'You haven\'t entered any %1$scontent%2$s yet!  As you start writing your content, we\'ll make recommendations for better SEO!', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content-length" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'badShort'  => sprintf( __( 'The content should be longer, we recommend %1$sat least 300 words%2$s. Try writing more about the focus keyword of this page.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content-length" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'ok'   => sprintf( __( 'We recommend a %1$sminimum of 300 words%2$s for the best SEO results.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content-length" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'good' => sprintf( __( 'Your content is over the recommended %1$sminimum of 300 words%2$s, good job!', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content-length" target="_blank">',
+			'</a>'
+		),
 		// Max value.
 		'badShortScore' => 199,
-		'ok'   => __( 'We recommend a minimum of 300 words for the best SEO results.', 'bgseo' ),
 		// Max value.
 		'okScore' => 300,
-		'good' => __( 'Your content is over the recommended minimum of 300 words, good job!', 'bgseo' ),
 	),
 	// Since there's no _n() in the js implementation, I'll just add a singular and plural translation for now.
 	'keywordUsage' => array(
