dd6bd8647c1c88db0bbd2499f300ef99c6b73bf2
Tim <dev@tim.ph>
2017-01-04 16:35:07 -0500

    add image usage recommendation inline links to statuses

--- includes/configs/i18n/image.config.php
+++ includes/configs/i18n/image.config.php
@@ -1,7 +1,15 @@
 <?php
 return array(
 	'length' => array(
-		'good' => __( 'Your article contains at least one image, which is great for SEO, awesome!', 'bgseo' ),
-		'bad' => __( 'Try adding at least one image that\'s relevant to your content\'s topic to further optimize your page.', 'bgseo' ),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'good' => sprintf( __( 'Your article %1$scontains at least one image%2$s, which is great for SEO, awesome!', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#images" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'bad' => sprintf( __( 'Try adding %1$sat least one image%2$s that\'s relevant to your content\'s topic to further optimize your page.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#images" target="_blank">',
+			'</a>'
+		),
 	),
 );
