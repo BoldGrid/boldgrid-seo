52a976d6b5cb90b0b4dd802421265e8cf741a834
Tim <dev@tim.ph>
2017-01-04 16:31:41 -0500

    adding inline links for keyword usage in content

--- includes/configs/i18n/content.config.php
+++ includes/configs/i18n/content.config.php
@@ -12,15 +12,44 @@ return array(
 		'okScore' => 300,
 		'good' => __( 'Your content is over the recommended minimum of 300 words, good job!', 'bgseo' ),
 	),
+	// Since there's no _n() in the js implementation, I'll just add a singular and plural translation for now.
 	'keywordUsage' => array(
-		// Since there's no _n() in the js implementation, I'll just add a singular and plural translation for now.
-		/** Translators: %s is equal to the count that the keyword should appear. **/
-		'bad'  => __( 'You haven\'t used your keyword in your content at all.  Try adding it naturally by talking about the subject more.', 'bgseo' ),
-		'okShortSingular'   => __( 'Your keyword is being used in your content, but we recommend using it at least 1 time.', 'bgseo' ),
-		'okShort'   => __( 'Your keyword is being used in your content, but we recommend using it at least %s times.', 'bgseo' ),
-		'okLongSingular'   => __( 'Your keyword appears excessively in your content.  Try to only use it 1 time and use other words and variations that are related to it.', 'bgseo' ),
-		'okLong'   => __( 'Your keyword appears excessively in your content.  Try to only use it %s times and use other words and variations that are related to it.', 'bgseo' ),
-		'goodSingular' => __( 'Great, you have included your keyword at least 1 time in your content. This helps get you a better SEO score!', 'bgseo' ),
-		'good' => __( 'Great, you have included your keyword at least %s times in your content. This helps get you a better SEO score!', 'bgseo' ),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'bad'  => sprintf( __( 'You haven\'t used your %1$skeyword in your content%2$s at all.  Try adding it naturally by talking about the subject more.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'okShortSingular' => sprintf( __( 'Your %1$skeyword is being used in your content%2$s, but we recommend using it at least 1 time.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
+			'</a>'
+		),
+
+		/* translators: 1: opening <a> tag 2: closing </a> tag %%s: is equal to the count that the keyword should appear and added by JS */
+		'okShort' => sprintf( __( 'Your %1$skeyword is being used in your content%2$s, but we recommend using it at least %%s times.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
+			'</a>'
+		),
+
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'okLongSingular' => sprintf( __( 'Your %1$skeyword appears excessively in your content%2$s.  Try to only use it 1 time and use other words and variations that are related to it.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag %%s: is equal to the count that the keyword should appear and added by JS */
+		'okLong'   => sprintf( __( 'Your %1$skeyword appears excessively in your content%2$s.  Try to only use it %%s times and use other words and variations that are related to it.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'goodSingular' => sprintf( __( 'Great, you have included the %1$skeyword in your content%2$s at least 1 time. This helps get you a better SEO score!', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag %%s: is equal to the count that the keyword should appear and added by JS */
+		'good' => sprintf( __( 'Great, you have included the %1$skeyword in your content%2$s at least %%s times. This helps get you a better SEO score!', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
+			'</a>'
+		),
 	),
 );
