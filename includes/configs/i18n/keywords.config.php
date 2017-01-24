aded469ef7a3183aa1a14a324e422d9dd8927649
Tim <dev@tim.ph>
2017-01-24 16:51:26 -0500

    adding links to keyword phrase length messages

--- includes/configs/i18n/keywords.config.php
+++ includes/configs/i18n/keywords.config.php
@@ -11,8 +11,20 @@ return array(
 		'max' => 2.5,
 	),
 	'keywordPhrase' => array(
-		'good' => __( 'Great, you\'ve entered a keyword phrase for the focus of your content!', 'bgseo' ),
-		'ok' => __( 'It looks like you have entered a single word for the keyword.  We recommend adding a keyword phrase instead of a single word for better results.', 'bgseo' ),
-		'bad' => __( 'You haven\'t entered a keyword or keyword phrase for the focus of this content.  This helps guide you in writing better optimized content!', 'bgseo' ),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'good' => sprintf( __( 'Great, you\'ve entered a %1$skeyword phrase%2$s for the focus of your content!', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#keyword-phrase-length" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'ok' => sprintf( __( 'It looks like you have entered a single word for the keyword.  We recommend adding a %1$skeyword phrase%2$s instead of a single word for better results.', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#keyword-phrase-length" target="_blank">',
+			'</a>'
+		),
+		/* translators: 1: opening <a> tag 2: closing </a> tag */
+		'bad' => sprintf( __( 'You haven\'t entered a %1$skeyword phrase%2$s for the focus of this content.  This helps guide you in writing better optimized content!', 'bgseo' ),
+			'<a href="https://boldgrid.com/support/seo/keywords#keyword-phrase-length" target="_blank">',
+			'</a>'
+		),
 	),
 );
