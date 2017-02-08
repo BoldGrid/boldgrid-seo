72816e60b297e42906636aa5ffa6b4e21d0d90a7
Tim <dev@tim.ph>
2017-02-08 17:59:38 -0500

    updating keyword to keyword phrase in lang

--- includes/configs/i18n/content.config.php
+++ includes/configs/i18n/content.config.php
@@ -9,7 +9,7 @@ return array(
 			'</a>'
 		),
 		/* translators: 1: opening <a> tag 2: closing </a> tag */
-		'badShort'  => sprintf( __( 'The content should be longer, we recommend %1$sat least 300 words%2$s. Try writing more about the focus keyword of this page.', 'bgseo' ),
+		'badShort'  => sprintf( __( 'The content should be longer, we recommend %1$sat least 300 words%2$s. Try writing more about the focus keyword phrase of this page.', 'bgseo' ),
 			'<a href="https://boldgrid.com/support/seo/keywords#content-length" target="_blank">',
 			'</a>'
 		),
@@ -31,29 +31,29 @@ return array(
 	// Since there's no _n() in the js implementation, I'll just add a singular and plural translation for now.
 	'keywordUsage' => array(
 		/* translators: 1: opening <a> tag 2: closing </a> tag */
-		'bad'  => sprintf( __( 'You haven\'t used your %1$skeyword in your content%2$s at all.  Try adding it naturally by talking about the subject more.', 'bgseo' ),
+		'bad'  => sprintf( __( 'You haven\'t used your %1$skeyword phrase in your content%2$s at all.  Try adding it naturally by talking about the subject more.', 'bgseo' ),
 			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
 			'</a>'
 		),
 		/* translators: 1: opening <a> tag 2: closing </a> tag */
-		'okShortSingular' => sprintf( __( 'Your %1$skeyword is being used in your content%2$s, but we recommend using it at least 1 time.', 'bgseo' ),
+		'okShortSingular' => sprintf( __( 'Your %1$skeyword phrase is being used in your content%2$s, but we recommend using it at least 1 time.', 'bgseo' ),
 			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
 			'</a>'
 		),
 
 		/* translators: 1: opening <a> tag 2: closing </a> tag %%s: is equal to the count that the keyword should appear and added by JS */
-		'okShort' => sprintf( __( 'Your %1$skeyword is being used in your content%2$s, but we recommend using it at least %%s times.', 'bgseo' ),
+		'okShort' => sprintf( __( 'Your %1$skeyword phrase is being used in your content%2$s, but we recommend using it at least %%s times.', 'bgseo' ),
 			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
 			'</a>'
 		),
 
 		/* translators: 1: opening <a> tag 2: closing </a> tag */
-		'okLongSingular' => sprintf( __( 'Your %1$skeyword appears excessively in your content%2$s.  Try to only use it 1 time and use other words and variations that are related to it.', 'bgseo' ),
+		'okLongSingular' => sprintf( __( 'Your %1$skeyword phrase appears excessively in your content%2$s.  Try to only use it 1 time and use other words and variations that are related to it.', 'bgseo' ),
 			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
 			'</a>'
 		),
 		/* translators: 1: opening <a> tag 2: closing </a> tag %%s: is equal to the count that the keyword should appear and added by JS */
-		'okLong'   => sprintf( __( 'Your %1$skeyword appears excessively in your content%2$s.  Try to only use it %%s times and use other words and variations that are related to it.', 'bgseo' ),
+		'okLong'   => sprintf( __( 'Your %1$skeyword phrase appears excessively in your content%2$s.  Try to only use it %%s times and use other words and variations that are related to it.', 'bgseo' ),
 			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
 			'</a>'
 		),
@@ -63,7 +63,7 @@ return array(
 			'</a>'
 		),
 		/* translators: 1: opening <a> tag 2: closing </a> tag %%s: is equal to the count that the keyword should appear and added by JS */
-		'good' => sprintf( __( 'Great, you have included the %1$skeyword in your content%2$s at least %%s times. This helps get you a better SEO score!', 'bgseo' ),
+		'good' => sprintf( __( 'Great, you have included the %1$skeyword phrase in your content%2$s at least %%s times. This helps get you a better SEO score!', 'bgseo' ),
 			'<a href="https://boldgrid.com/support/seo/keywords#content" target="_blank">',
 			'</a>'
 		),
