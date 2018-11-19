958c56b7a89f6f9d4b02065084c448bd4347e01a
Rafael Ramos <rramo012@gmail.com>
2018-11-19 15:43:09 -0500

    Build

--- assets/js/bgseo.js
+++ assets/js/bgseo.js
@@ -1547,10 +1547,10 @@ BOLDGRID.SEO = BOLDGRID.SEO || {};
 			wp.data.subscribe( _.debounce( function () {
 
 				// Make sure content is different before running analysis.
-				if ( wp.data.select( 'core/editor' ).getEditedPostAttribute( 'content' ) !== latestContent ) {
+				if ( self.getRawText() !== latestContent ) {
 					api.Wordcount.update();
 					api.Editor.triggerAnalysis();
-					latestContent = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'content' );
+					latestContent = self.getRawText();
 				}
 			}, 1000 ) );
 		}
